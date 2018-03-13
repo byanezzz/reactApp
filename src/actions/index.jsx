f80cee43b8d240417f78ef1e20e5e2e32fe114c6
byanezzz <biancayanez89@gmail.com>
2018-03-13 09:20:13 -0300

    Updates

--- src/actions/index.jsx
+++
@@ -1,3 +0,0 @@
-//actionCreator
-export const SET_CITY = 'SET_CITY'
-export const setCity = (value) => ({ type: SET_CITY, value })
--- src/store/index.jsx
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-import { city } from './../reducers/city';
-const initialState = {
-  city: 'Barquisimeto,ve'
-}
-export const store = createStore(city, initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__());
